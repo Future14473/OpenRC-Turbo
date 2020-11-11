486d595b99cd0997b7283de853fa3c22e6a0f427
Divya Ponnuvelu <divya.ponnuv@gmail.com>
2020-11-11 14:26:54 -0800

    Divya_Branch-9-11-2020

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mecanum {
+    double topLeftPower;
+    double topRightPower;
+    double bottomLeftPower;
+    double bottomRightPower;
+    double f;
+    double s;
+    DcMotorEx topRightDrive;
+    DcMotorEx topLeftDrive;
+    DcMotorEx bottomRightDrive;
+    DcMotorEx bottomLeftDrive;
+
+
+    public Mecanum(){
+
+    }
+
+    public Mecanum(HardwareMap hardwareMap ){
+        topLeftDrive  = hardwareMap.get(DcMotorEx.class, "top_left_drive");
+        topRightDrive = hardwareMap.get(DcMotorEx.class, "top_right_drive");
+        bottomLeftDrive = hardwareMap.get(DcMotorEx.class, "bottom_left_drive");
+        bottomRightDrive = hardwareMap.get(DcMotorEx.class, "bottom_right_drive");
+
+        topLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        bottomLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        topRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        bottomRightDrive.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+    public void set_motor_values(double F, double S, double turn){
+        topRightPower = F - S + turn;
+        bottomRightPower = F + S + turn;
+        topLeftPower = F + S - turn;
+        bottomLeftPower = F - S - turn;
+    }
+
+    public void set_forward_strafe(double angle){
+        angle = Math.toRadians(angle);
+        f = Math.sin(angle);
+        s = Math.cos(angle);
+    }
+
+    public void move(  double f, double s, double turn){
+//        set_forward_strafe(angle);
+        set_motor_values(f, s, turn);
+        topRightDrive.setVelocity(topRightPower);
+        topLeftDrive.setVelocity(topLeftPower);
+        bottomLeftDrive.setVelocity(bottomLeftPower);
+        bottomRightDrive.setVelocity(bottomRightPower);
+        topRightDrive.setVelocity(topRightPower);
+        topLeftDrive.setVelocity(topLeftPower);
+        bottomLeftDrive.setVelocity(bottomLeftPower);
+        bottomRightDrive.setVelocity(bottomRightPower);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Drivetrain_Auto_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Drivetrain_Auto_OpMode.java
@@ -27,12 +27,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -51,35 +50,25 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Basic: Iterative OpMode", group="Iterative Opmode")
+@Autonomous(name="Mecanum_Drivetrain_Auto", group="Iterative Opmode")
 @Disabled
-public class BasicOpMode_Iterative extends OpMode
+public class Mecanum_Drivetrain_Auto_OpMode extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
-
+    Mecanum Motors = new Mecanum();
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
+        Mecanum Motors = new Mecanum(hardwareMap);
+
+
     }
 
     /*
@@ -87,6 +76,7 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void init_loop() {
+
     }
 
     /*
@@ -102,37 +92,28 @@ public class BasicOpMode_Iterative extends OpMode
      */
     @Override
     public void loop() {
+        Motors.move(0, 0, 1);
+
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        double topLeftPower;
+        double topRightPower;
+        double bottomLeftPower;
+        double bottomRightPower;
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
-
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
         double drive = -gamepad1.left_stick_y;
         double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+        topLeftPower = Range.clip(drive - turn, -1.0, 1.0) ;
+        topRightPower = Range.clip(-drive + turn, -1.0, 1.0) ;
+        bottomLeftPower = Range.clip(-drive - turn, -1.0, 1.0) ;
+        bottomRightPower = Range.clip( drive + turn, -1.0, 1.0) ;
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Motors", "left (%.2f), right (%.2fx`)");
     }
 
     /*
-     * Code to run ONCE after the driver hits STOP
-     */
+3     */
     @Override
     public void stop() {
     }
